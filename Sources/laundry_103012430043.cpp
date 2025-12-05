f90d2048724e20e86d25fe2e03a851caf685907c
Akbar Rizqullah Putra Susanto <akbarrizqullah228@gmail.com>
2025-12-05 16:09:06 +0700

    update code laundry

--- Sources/laundry_103012430043.cpp
+++ Sources/laundry_103012430043.cpp
@@ -53,3 +53,46 @@ void insertAfterLaundry(addressCustomer &C, addressLaundry Prec, addressLaundry 
         Prec->next = P;
     }
 }
+
+addressLaundry searchLaundry(addressCustomer C, string layanan){
+    addressLaundry P;
+    P = C->firstLaundry;
+    if (C == nullptr){
+        cout << "Tidak ada nama customer" << endl;
+    } else {
+        while (P != nullptr) {
+            if (P->info.layanan == layanan) {
+                cout << P << endl;
+            }
+            P = P->next;
+        }
+    }
+}
+
+void printLaundry(addressCustomer C){
+    addressLaundry P;
+    int idx;
+    P = C->firstLaundry;
+    idx = 1;
+    if (C == nullptr){
+        cout << "Customer tidak ditemukan." << endl;
+    }else if (P == nullptr){
+        cout << "   Tidak ada laundry terdaftar untuk customer " << C->info.name << "." << endl;
+    }else {
+        cout << "   Laundry milik: " << C->info.name << endl;
+        cout << "   ----------------------------------------" << endl;
+        while (P != nullptr) {
+            cout << "   Laundry #" << idx++ << endl;
+            cout << "      Nama Pakaian : " << P->info.name << endl;
+            cout << "      No. Tlp      : " << P->info.noTlp << endl;
+            cout << "      Alamat       : " << P->info.alamat << endl;
+            cout << "      Berat (kg)   : " << P->info.beratPakaian << endl;
+            cout << "      Layanan      : " << P->info.layanan << endl;
+            cout << "      Harga        : Rp" << P->info.harga << endl;
+            cout << "      Jenis Pakaian: " << P->info.jenisPakaian << endl;
+            cout << "   ----------------------------------------" << endl;
+
+            P = P->next;
+        }
+    }
+}
